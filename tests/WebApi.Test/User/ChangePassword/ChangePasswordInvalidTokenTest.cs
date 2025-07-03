0c9f077127d9ba37315b97ad5f4c9b94e2734075
Guilherme Mateus <guilherme.mat.ds@gmail.com>
2025-07-03 16:10:23 -0300

    test: adding integration test to change password

---
+++ tests/WebApi.Test/User/ChangePassword/ChangePasswordInvalidTokenTest.cs
@@ -0,0 +1,46 @@
+﻿using CommonTestUtilities.Requests;
+using CommonTestUtilities.Tokens;
+using Shouldly;
+using System.Net;
+
+namespace WebApi.Test.User.ChangePassword
+{
+    public class ChangePasswordInvalidTokenTest : InventoryControlClassFixture
+    {
+        private const string METHOD = "user/change-password";
+
+        public ChangePasswordInvalidTokenTest(CustomWebApplicationFactory factory) : base(factory) { }
+
+        [Fact]
+        public async Task Error_Invalid_Token()
+        {
+            var request = RequestChangePasswordJsonBuilder.Build();
+
+            var response = await DoPut(method: METHOD, request: request, token: "invalidToken");
+
+            response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task Error_Without_Token()
+        {
+            var request = RequestChangePasswordJsonBuilder.Build();
+
+            var response = await DoPut(method: METHOD, request: request, token: string.Empty);
+
+            response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task Error_Token_With_User_NotFound()
+        {
+            var request = RequestChangePasswordJsonBuilder.Build();
+
+            var token = JwtTokenGeneratorBuilder.Build().Generate(Guid.NewGuid());
+
+            var response = await DoPut(method: METHOD, request: request, token: token);
+
+            response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+        }
+    }
+}
---
+++ tests/WebApi.Test/User/ChangePassword/ChangePasswordTest.cs
@@ -0,0 +1,77 @@
+﻿using CommonTestUtilities.Requests;
+using CommonTestUtilities.Tokens;
+using InventoryControl.Communication.Requests;
+using InventoryControl.Exceptions;
+using Shouldly;
+using System.Net;
+using System.Text.Json;
+
+namespace WebApi.Test.User.ChangePassword
+{
+    public class ChangePasswordTest : InventoryControlClassFixture
+    {
+        private const string METHOD = "user/change-password";
+
+        private readonly string _password;
+        private readonly string _email;
+        private readonly Guid _userIdentifier;
+
+        public ChangePasswordTest(CustomWebApplicationFactory factory) : base(factory)
+        {
+            _password = factory.GetPassword();
+            _email = factory.GetEmail();
+            _userIdentifier = factory.GetUserIdentifier();
+        }
+
+        [Fact]
+        public async Task Success()
+        {
+            var token = JwtTokenGeneratorBuilder.Build().Generate(_userIdentifier);
+
+            var request = RequestChangePasswordJsonBuilder.Build();
+            request.Password = _password;
+
+            var response = await DoPut(method: METHOD, request: request, token: token);
+            response.StatusCode.ShouldBe(HttpStatusCode.NoContent);
+
+            var loginRequest = new RequestLoginJson
+            {
+                Email = _email,
+                Password = _password
+            };
+
+            response = await DoPost(method: "login", request: loginRequest);
+            response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+
+            loginRequest.Password = request.NewPassword;
+
+            response = await DoPost(method: "login", request: loginRequest);
+            response.StatusCode.ShouldBe(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task Error_NewPassword_Empty()
+        {
+            var token = JwtTokenGeneratorBuilder.Build().Generate(_userIdentifier);
+
+            var request = new RequestChangePasswordJson
+            {
+                Password = _password,
+                NewPassword = string.Empty
+            };
+
+            var response = await DoPut(method: METHOD, request: request, token: token);
+
+            response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+
+            await using var responseBody = await response.Content.ReadAsStreamAsync();
+
+            var responseData = await JsonDocument.ParseAsync(responseBody);
+
+            var errors = responseData.RootElement.GetProperty("errors").EnumerateArray();
+
+            errors.Count().ShouldBe(1);
+            errors.ShouldContain(c => c.GetString()!.Equals(ResourceMessagesException.EMPTY_PASSWORD));
+        }
+    }
+}
